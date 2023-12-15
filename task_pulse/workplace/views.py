4778d8b9b3435960e365e0990248256e39b287f7
Maksim <umerenkovmaksim7@gmail.com>
2023-12-15 12:12:48 +0300

    refactor: "create task" widget added to tasks page

--- task_pulse/workplace/views.py
+++ task_pulse/workplace/views.py
@@ -1,5 +1,7 @@
 from django.contrib import messages
+from django.http import HttpResponseRedirect
 from django.shortcuts import redirect
+from django.urls import reverse_lazy
 from django.utils.translation import gettext as _
 from django.views import generic
 
@@ -18,17 +20,33 @@ class HomeCompanyView(
 
 class TaskList(
     mixins.CompanyUserRequiredMixin,
+    generic.edit.FormMixin, 
     generic.ListView,
 ):
     template_name = "workplace/tasks.html"
     model = models.Task
     context_object_name = "tasks"
+    form_class = forms.TaskCreationForm 
 
     def get_queryset(self):
         return models.Task.objects.filter(
             responsible=self.get_company_user(
                 self.request.resolver_match.kwargs["company_id"],
             ),
+        )
+    
+    def get_form(self, form_class=None):
+        if form_class is None:
+            form_class = self.get_form_class()
+
+        company_id = self.request.resolver_match.kwargs["company_id"]
+        company_user = self.get_company_user(company_id)
+        task = models.Task(author=company_user)
+
+        return form_class(
+            **self.get_form_kwargs(),
+            instance=task,
+            author=company_user,
         )
 
 
@@ -37,21 +55,24 @@ class TaskCreationForm(
     generic.FormView,
 ):
     form_class = forms.TaskCreationForm
-    template_name = "workplace/task_create_form.html"
 
     def post(self, request, *args, **kwargs):
         form = self.get_form()
         if form.is_valid():
             form.save()
             messages.success(request, _("Task created successfully!"))
+            return HttpResponseRedirect(reverse_lazy(
+                "workplace:tasks",
+                args=(request.resolver_match.kwargs["company_id"],),
+            ))
         else:
-            print(form.cleaned_data)
             messages.error(request, _("Invalid data"))
+            return self.render_to_response(self.get_context_data(form=form))
 
-        return redirect(
-            "workplace:create_task",
-            company_id=self.request.resolver_match.kwargs["company_id"],
-        )
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["form"] = self.get_form()
+        return context
 
     def get_form(self, form_class=None):
         if form_class is None:
