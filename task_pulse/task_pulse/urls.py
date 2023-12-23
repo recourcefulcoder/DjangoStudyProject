140956684de977593c28e48f6fbd953dcc761cc8
Maksim <umerenkovmaksim7@gmail.com>
2023-12-23 16:57:09 +0300

    fix: last fix

--- task_pulse/task_pulse/urls.py
+++ task_pulse/task_pulse/urls.py
@@ -10,4 +10,10 @@ urlpatterns = [
     path("admin/", admin.site.urls),
 ]
 
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns += (path("__debug__/", include(debug_toolbar.urls)),)
+
+
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
